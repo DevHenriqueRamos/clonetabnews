5a24a4da9e7bb835d193a825e17877b95ecb25b9
Henrique Ramos <devhenrique.ramos@gmail.com>
2025-12-29 12:53:17 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+function Home() {
+    return <>
+        <h1>Bug no meu Sistema</h1>
+        <p>Eu, programador de noites insones,
+            teclado suado e tela acesa,
+            vejo teus cachinhos danados na minha frente,
+            cada espiral um bug que eu quero debuggar com a boca.Eles pulam rebeldes, provocam erro 404 no meu controle,
+            faço loop infinito: dedos enroscando, puxando,
+            até o sistema travar de tanto desejo.Teus cachinhos danados são o código mais safado que já vi,
+            compilo teu gemido linha por linha,
+            e rodo sem break até o gozo crashar o servidor inteiro.
+        </p>
+    </>
+}
+
+export default Home;
