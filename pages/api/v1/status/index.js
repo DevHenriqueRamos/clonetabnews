49de29e2ebef9d2753e0c722ed9fa060f48a9736
Henrique Ramos <devhenrique.ramos@gmail.com>
2026-01-05 18:34:01 +0000

    add `database.js` and env files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,5 @@
-export default function status(request, response) {
+import database from "../../../../infra/database.js";
+
+export default async function status(request, response) {
   response.status(200).json({ chave: "são acima da média" });
 }
