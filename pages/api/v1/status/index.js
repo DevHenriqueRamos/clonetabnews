dbef95a5d59f35651445d1e7a32225a4d3fee463
Henrique Ramos <devhenrique.ramos@gmail.com>
2026-01-06 15:58:00 +0000

    add `jsconfig.json` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
-export default async function status(request, response) {
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result.rows);
   response.status(200).json({ chave: "são acima da média" });
 }
+
+export default status;
